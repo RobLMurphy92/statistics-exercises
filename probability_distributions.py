2fd56049bef3eef873b6f1e104f50d142d470578
Robert <rob.l.murphy92@gmail.com>
2021-05-07 11:14:29 -0500

    updated answers and included ravinder solutions

--- probability_distributions.py
+++ probability_distributions.py
@@ -4,6 +4,12 @@
 # In[1]:
 
 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -20,22 +26,52 @@ from scipy import stats
 # 
 # fixed number of observations
 
+# ### Coin toss interview question
+# 
+# - how do you insure a fair toss when either you dont know that coin is fair /ubnfair or assuming the coin is unfair?
+# 
+# - Just toss the coin twice!!
+
+# p(T)= 1/4
+# p(H)=3/4
+# 
+# 
+# P(H,H) = 3/4 * 3/4
+# 
+# P(H,T) = 3/4 * 1/4
+# 
+# P(T,H) = 1/4 * 3/4
+# 
+# P(T,T) = 1/4 * 1/4
+
 # ### 1.)A bank found that the average number of cars waiting during the noon hour at a drive-up window follows a Poisson distribution with a mean of 2 cars. Make a chart of this distribution and answer these questions concerning the probability of cars waiting at the drive-up window.
 # 
 # 
 
-# In[3]:
+# In[4]:
 
 
 λ = 2 #cars at noon
 
 
+# In[7]:
+
+
+x = np.arange(0,12)
+y = stats.poisson(λ).pmf(x)
+
+plt.bar(x,y)
+plt.title('Poisson dist with $lambda = 2$')
+plt.xlabel('Number of cars')
+plt.ylabel('P(X)');
+
+
 # a.) What is the probability that no cars drive up in the noon hour?
 
-# In[12]:
+# In[8]:
 
 
-stats.poisson(λ).pmf(12)
+stats.poisson(λ).pmf(0)
 
 
 # b.) What is the probability that 3 or more cars come through the drive through?
@@ -48,27 +84,39 @@ stats.poisson(λ).sf(2)
 
 # c.) How likely is it that the drive through gets at least 1 car? means one or more
 
-# In[14]:
+# In[9]:
 
 
-stats.poisson(λ).sf(0)
+stats.poisson(λ).sf(0).round(2)
+
+
+# In[ ]:
+
+
+
 
 
 # ### 2.)Grades of State University graduates are normally distributed with a mean of 3.0 and a standard deviation of .3. Calculate the following:
 # 
 
+# In[ ]:
+
+
+
+
+
 # - a.) What grade point average is required to be in the top 5% of the graduating class?
 
-# In[16]:
+# In[11]:
 
 
 #top 15 %
 mean = 3
 stddev = .3
-stats.norm(mean,stddev).isf(.05)
+stats.norm(mean,stddev).isf(.05).round(2)
 
 
-# - b.) What GPA constitutes the bottom 15% of the class?
+# - b.) What GPA constitutes the bottom 15% of the class? # can use ppf
 
 # In[27]:
 
@@ -76,6 +124,12 @@ stats.norm(mean,stddev).isf(.05)
 mean = 3
 stddev = .3
 stats.norm(mean,stddev).isf(.85)
+
+
+# In[13]:
+
+
+stats.norm(mean,stddev).ppf(.15)
 
 
 # - c.)An eccentric alumnus left scholarship money for students in the third decile from the bottom of their class. Determine the range of the third decile. Would a student with a 2.8 grade point average qualify for this scholarship?
@@ -88,10 +142,22 @@ stddev = .3
 stats.norm(mean,stddev).isf(.70)
 
 
+# In[ ]:
+
+
+
+
+
 # In[31]:
 
 
 stats.norm(mean,stddev).isf(.80)
+
+
+# In[15]:
+
+
+stats.norm(mean,stddev).ppf([0.2,0.3])
 
 
 # In[33]:
@@ -109,28 +175,57 @@ stats.norm(mean,stddev).isf(.80)
 stats.norm(mean,stddev).isf(.05)
 
 
+# In[16]:
+
+
+stats.norm(mean,stddev).cdf(3.5)
+
+
+# In[17]:
+
+
+(np.random.normal(3,0.3,100_000) < 3.5).mean()
+
+
 # ### 3.)A marketing website has an average click-through rate of 2%. One day they observe 4326 visitors and 97 click-throughs. 
 # 
 
 # - a.)How likely is it that this many people or more click through?
 
-# In[43]:
+# In[19]:
 
 
-#trials 
+#binomal
 ct = 97
 n_trials = 4326
 p = .02
 
 clicks = stats.binom(n_trials, p)
+clicks.sf(96)
+
+
+# In[20]:
+
+
+#using simulation
+clicks = np.random.choice([0,1], (100_000, 4326), p = [0.98, 0.02])
 clicks
 
 
-# In[45]:
+# In[21]:
 
 
-likely =clicks.sf(ct-1)
-likely
+(clicks.sum(axis =1) >96).mean()
+
+
+# In[23]:
+
+
+# using poisson
+
+λ = n_trials * p
+
+stats.poisson(λ).sf(96)
 
 
 # ### 4.)You are working on some statistics homework consisting of 100 questions where all of the answers are a probability rounded to the hundreths place. Looking to save time, you put down random probabilities as the answer to each question.
@@ -138,13 +233,21 @@ likely
 
 # - a.) What is the probability that at least one of your first 60 answers is correct?
 
-# In[47]:
+# In[29]:
 
 
-h_trials = 100
-p =0.01
-chance = stats.binom(h_trials,p).cdf(60)
+#binom
+h_trials = 60
+pt =0.01
+chance = stats.binom(h_trials,pt).sf(0)
 chance
+
+
+# In[31]:
+
+
+#by simulation
+((np.random.choice([0,1], (100_000, 60), p =[0.99, 0.01])).sum(axis=1) >0).mean()
 
 
 # ### 5.)The codeup staff tends to get upset when the student break area is not cleaned up. Suppose that there's a 3% chance that any one student cleans the break area when they visit it, and, on any given day, about 90% of the 3 active cohorts of 22 students visit the break area. 
@@ -152,64 +255,61 @@ chance
 
 # - a.) How likely is it that the break area gets cleaned up each day? 
 
-# In[70]:
+# In[33]:
 
 
 number_of_students = (.90 * (3*22))
 p = 0.03
 
 
-# In[71]:
+# In[35]:
 
 
-number_of_students
+number_of_students = round(number_of_students)
+number_of_students 
 
 
-# In[72]:
+# In[37]:
 
 
-cd_trials = stats.binom(number_of_students, p)
+cd_trials = stats.binom(number_of_students, p).sf(0)
 cd_trials
 
 
-# In[76]:
+# In[42]:
 
 
-likely_cleaned = cd_trials.sf(0)
-likely_cleaned
+x = np.arange(0,10)
+y = stats.binom(number_of_students, p).pmf(x)
 
-
-# In[84]:
-
-
-1- cd_trials.pmf(0)
+plt.bar(x,y)
+plt.xlabel('Number of time are is cleaned per day')
 
 
 # - b.) How likely is it that it goes two days without getting cleaned up? All week?
 
-# In[78]:
+# In[46]:
 
 
-never_cleaned = 1-likely_cleaned
+never_cleaned = 1-cd_trials
 never_cleaned
 
 
-# In[83]:
+# In[49]:
 
 
-cd_trials.pmf(0)
+# two_days
+
+cd_trials_2_days = stats.binom(number_of_students * 2, p).pmf(0)
+cd_trials_2_days
 
 
-# In[87]:
+# In[50]:
 
 
-((cd_trials.pmf(0))**2).round(3)
-
-
-# In[91]:
-
-
-((cd_trials.pmf(0))**7).round(6) #.000003
+#all week
+cd_trials_5_days = stats.binom(number_of_students * 5, p).pmf(0)
+cd_trials_5_days
 
 
 # ### 6.)You want to get lunch at La Panaderia, but notice that the line is usually very long at lunchtime. After several weeks of careful observation, you notice that the average number of people in line when your lunch break starts is normally distributed with a mean of 15 and standard deviation of 3. If it takes 2 minutes for each person to order, and 10 minutes from ordering to getting your food, what is the likelihood that you have at least 15 minutes left to eat your food before you have to go back to class? Assume you have one hour for lunch, and ignore travel time to and from La Panaderia.
@@ -220,7 +320,7 @@ cd_trials.pmf(0)
 
 
 
-# In[116]:
+# In[54]:
 
 
 line_stdev = 3
@@ -231,29 +331,46 @@ time_cook = 10 #min
 eat_time = 15
 
 
-
-time_to_get_order = lunch_break - time_cook - order_time_person - eat_time
+#max amount of time without being late. 60- 15 eat - 10 food prep
+time_to_get_order = lunch_break - time_cook - eat_time
 time_to_get_order
 
 
-# In[113]:
+# In[52]:
 
 
-mean_line = (15*2)
+#convert mean and std from ppl to min
+mean_line = (15*2) #mins
 std_line = (3*2)
 
 
-# In[114]:
+# In[55]:
 
 
-stats.norm(mean_line, std_line).cdf(time_eat)
+stats.norm(mean_line, std_line).cdf(time_to_get_order)
+
+
+# In[56]:
+
+
+mean = 15
+std_dev = 3
+
+stats.norm(mean,std_dev).cdf(17.5)
+
+
+# In[57]:
+
+
+(np.random.normal(30,6,100_000) < 35).mean()
 
 
 # ### 7.)Connect to the employees database and find the average salary of current employees, along with the standard deviation. For the following questions, calculate the answer based on modeling the employees salaries with a normal distribution defined by the calculated mean and standard deviation then compare this answer to the actual values present in the salaries dataset.
 # 
 # 
 
-# In[119]:
+# In[6]:
+
 
 from env import host, user, password
 
@@ -339,10 +456,13 @@ eighty
 1-sixity_five - eighty
 
 
+between_six_eig = np.diff(stats.norm(salaries_mean,salaries_std).cdf([65000, 80000]))
+between_six_eig
+
 # - d.)What do the top 5% of employees make?
 
 # In[27]:
 
 
 stats.norm(salaries_mean,salaries_std).isf(.05)
-# In[ ]:
+
